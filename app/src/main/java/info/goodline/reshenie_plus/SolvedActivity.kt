5eebbb953369d32a795947f6329b22032966fc82
Izaky <Izakynash@gmail.com>
2019-03-27 00:58:01 +0700

    second

---
+++ app/src/main/java/info/goodline/reshenie_plus/SolvedActivity.kt
@@ -0,0 +1,12 @@
+package info.goodline.reshenie_plus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SolvedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solved)
+    }
+}
