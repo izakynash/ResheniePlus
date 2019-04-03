81570f8752e449c041d54300ba726c468b6c3815
Izaky <Izakynash@gmail.com>
2019-04-03 13:07:51 +0700

    +editBookAct4

--- app/src/main/java/info/goodline/reshenie_plus/AllBooksActivity.kt
+++ app/src/main/java/info/goodline/reshenie_plus/AllBooksActivity.kt
@@ -2,18 +2,30 @@ package info.goodline.reshenie_plus
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import kotlinx.android.synthetic.main.activity_all_books.*
 
 class AllBooksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_books)
+        setSupportActionBar(toolbar);
     }
 
-   fun btnEditBook(view: View) {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.all_books_menu, menu)
+        return true
+}
+
+    fun btnEditBook(view: View) {
         val intent = Intent(this, EditBookActivity::class.java)
         startActivity(intent)
-    }
+   }
+
+
 }
