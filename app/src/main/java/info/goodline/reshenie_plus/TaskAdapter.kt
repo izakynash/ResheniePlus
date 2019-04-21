12537180b706d5c1bcf7e95867f39dd6f2b65e6e
Izaky <Izakynash@gmail.com>
2019-04-21 22:13:20 +0700

    added TaskActivity

--- app/src/main/java/info/goodline/reshenie_plus/TaskAdapter.kt
+++ app/src/main/java/info/goodline/reshenie_plus/TaskAdapter.kt
@@ -7,20 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-class TaskAdapter(private val bookNameArray: List<String>): RecyclerView.Adapter<TaskAdapter.TaskVH>() {
+class TaskAdapter(private val taskNumberArray: List<String>): RecyclerView.Adapter<TaskAdapter.TaskVH>() {
 
-    override fun getItemCount() = bookNameArray.size
+    override fun getItemCount() = taskNumberArray.size
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewInt: Int): TaskVH {
-        Log.d(TAG, "Chapter_onCreateViewHolder")
-        val itemView = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_view_numbers, viewGroup, false)
+        Log.d(TAG, "Task_onCreateViewHolder")
+        val itemView = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_view_simple_list, viewGroup, false)
         return TaskVH(itemView)
     }
 
     override fun onBindViewHolder(VH: TaskVH, position: Int) {
-        val taskNumber: String = bookNameArray[position]
+        val taskNumber: String = taskNumberArray[position]
         VH.tvTaskVH?.text = taskNumber
-        Log.d(TAG, "Chapter_onBindView")
+        Log.d(TAG, "Task_onBindView")
 
 //        VH.itemView.setOnClickListener {
 //            clickListener.onItemClick(chapterName)
